cc3de1b272aac03854fb4a6eb7fabae284436927
DariodAbate <dario.dabate@mail.polimi.it>
2022-03-22 22:35:23 +0100

    testing fixing mechanism

--- src/main/java/it/polimi/ingsw/Student.java
+++ src/main/java/it/polimi/ingsw/Student.java
@@ -11,82 +11,82 @@ public class Student {
     private Set<Color> pinkStudents;
 
     public Student() {
-        this.yellowStudents = new HashSet<Color>();
-        this.blueStudents = new HashSet<Color>();
-        this.redStudents = new HashSet<Color>();
-        this.greenStudents = new HashSet<Color>();
-        this.pinkStudents = new HashSet<Color>();
+        this.yellowStudents = new HashSet<>();
+        this.blueStudents = new HashSet<>();
+        this.redStudents = new HashSet<>();
+        this.greenStudents = new HashSet<>();
+        this.pinkStudents = new HashSet<>();
     }
 
     public void add(Color color) {
-        if(color.equals(Color.yellow)){
+        if(color.equals(Color.YELLOW)){
             yellowStudents.add(color);
         }
-        if (color.equals(Color.red)){
+        if (color.equals(Color.RED)){
             redStudents.add(color);
         }
-        if(color.equals(Color.blue)){
+        if(color.equals(Color.BLUE)){
             blueStudents.add(color);
         }
-        if(color.equals(Color.green)){
+        if(color.equals(Color.GREEN)){
             greenStudents.add(color);
         }
-        if(color.equals(Color.pink)){
+        if(color.equals(Color.PINK)){
             pinkStudents.add(color);
         }
     }
     public void add(Color color, int num){
         for (int i=0; i<num; i++) {
-            if (color.equals(Color.yellow)) {
+            if (color.equals(Color.YELLOW)) {
                 yellowStudents.add(color);
             }
-            if (color.equals(Color.red)) {
+            if (color.equals(Color.RED)) {
                 redStudents.add(color);
             }
-            if (color.equals(Color.blue)) {
+            if (color.equals(Color.BLUE)) {
                 blueStudents.add(color);
             }
-            if (color.equals(Color.green)) {
+            if (color.equals(Color.GREEN)) {
                 greenStudents.add(color);
             }
-            if (color.equals(Color.pink)) {
+            if (color.equals(Color.PINK)) {
                 pinkStudents.add(color);
             }
         }
     }
     public void remove(Color color){
-        if(color.equals(Color.yellow)){
+        if(color.equals(Color.YELLOW)){
             yellowStudents.remove(color);
         }
-        if (color.equals(Color.red)){
+        if (color.equals(Color.RED)){
             redStudents.remove(color);
         }
-        if(color.equals(Color.blue)){
+        if(color.equals(Color.BLUE)){
             blueStudents.remove(color);
         }
-        if(color.equals(Color.green)){
+        if(color.equals(Color.GREEN)){
             greenStudents.remove(color);
         }
-        if(color.equals(Color.pink)){
+        if(color.equals(Color.PINK)){
             pinkStudents.remove(color);
         }
 
     }
     public void remove(Color color, int num){
         for(int i=0; i<num; i++) {
-            if (color.equals(Color.yellow)) {
+            if (color.equals(Color.YELLOW)) {
                 yellowStudents.remove(color);
             }
-            if (color.equals(Color.red)) {
+            if (color.equals(Color.RED)) {
                 redStudents.remove(color);
             }
-            if (color.equals(Color.blue)) {
+            if (color.equals(Color.BLUE)) {
                 blueStudents.remove(color);
             }
-            if (color.equals(Color.green)) {
+            if (color.equals(Color.GREEN)) {
                 greenStudents.remove(color);
             }
-            if (color.equals(Color.pink)) {
+            if (color.equals(Color.PINK)) {
                 pinkStudents.remove(color);
             }
         }
@@ -97,19 +97,19 @@ public class Student {
         return yellowStudents.size()+blueStudents.size()+ pinkStudents.size()+ redStudents.size()+ blueStudents.size();
     }
     public int numStudents(Color color){
-        if (color.equals(Color.yellow)) {
+        if (color.equals(Color.YELLOW)) {
             return yellowStudents.size();
         }
-        if (color.equals(Color.red)) {
+        if (color.equals(Color.RED)) {
             return redStudents.size();
         }
-        if (color.equals(Color.blue)) {
+        if (color.equals(Color.BLUE)) {
             return blueStudents.size();
         }
-        if (color.equals(Color.green)) {
+        if (color.equals(Color.GREEN)) {
             return greenStudents.size();
         }
-        if (color.equals(Color.pink)) {
+        if (color.equals(Color.PINK)) {
             return pinkStudents.size();
         }
         //fare exception per colori non validi!
