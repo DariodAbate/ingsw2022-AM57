3669fd510a1c5fb6dedcc1b88a158689ffc57cfe
DariodAbate <dario.dabate@mail.polimi.it>
2022-03-22 20:50:55 +0100

    adding maven artifact

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -1,20 +1,11 @@
 package it.polimi.ingsw;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void theAnswer() {
+        assertEquals(42, 22 + 20);
     }
 }
