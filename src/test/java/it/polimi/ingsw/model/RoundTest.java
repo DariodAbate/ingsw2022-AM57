9dcafde5081a6cc2bd30b61072b4d6c84d08ef35
BrescianiLuca <luca5.bresciani@mail.polimi.it>
2022-04-08 12:57:12 +0200

    Fix nextTurn method, add tests

--- src/test/java/it/polimi/ingsw/model/RoundTest.java
+++ src/test/java/it/polimi/ingsw/model/RoundTest.java
@@ -1,7 +1,5 @@
 package it.polimi.ingsw.model;
 
-import it.polimi.ingsw.model.Player;
-import it.polimi.ingsw.model.Round;
 import it.polimi.ingsw.model.constantFactory.GameConstants;
 import it.polimi.ingsw.model.constantFactory.GameConstantsCreator;
 import it.polimi.ingsw.model.constantFactory.GameConstantsCreatorThreePlayers;
@@ -19,43 +17,13 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class RoundTest {
     GameConstantsCreator g;
-    /**
-     * This method tests that an Exception is thrown if the list of players
-     * didn't contain exactly 2 or 3 players.
-     */
-    @Test
-    void illegalNumberOfPlayers() {
-        g = new GameConstantsCreatorThreePlayers();
-        GameConstants gc = g.create();
-        ArrayList<Player> players = new ArrayList<>();
-        for (int i = 0; i < 5; i ++) {
-            players.add(new Player(i,gc));
-        }
-        assertThrows(IllegalArgumentException.class,
-                () -> {
-                    Round r = new Round(players);
-                });
-    }
 
     /**
-     * This method tests that an Exception in thrown if the given list
-     * of players is null.
-     */
-    @Test
-    void nullListOfPlayers() {
-        assertThrows(NullPointerException.class,
-                () -> {
-                    Round r = new Round(null);
-                });
-    }
-
-
-    /**
-     * This method tests the correct working of the method setActionOrder()
+     * This method tests the correct working of the method setActionPhaseOrder()
      * when there is a game with 3 players.
      */
     @Test
-    void setActionOrderTest3Player() {
+    void setActionPhaseOrderTest3Player() {
         g = new GameConstantsCreatorThreePlayers();
         GameConstants gc = g.create();
         ArrayList<Player> players = new ArrayList<>();
@@ -70,24 +38,24 @@ class RoundTest {
         players.add(p2);
         players.add(p3);
         r = new Round(players);
-        r.setActionOrder();
-        assertEquals(2, r.getActionOrder().get(0).getId());
+        r.setActionPhaseOrder();
+        assertEquals(2, r.getActionPhaseOrder().get(0).getId());
         p1.playCard(5);
         p2.playCard(7);
         p3.playCard(2);
         players.add(p1);
         players.add(p2);
         players.add(p3);
-        r.setActionOrder();
-        assertEquals(3, r.getActionOrder().get(0).getId());
+        r.setActionPhaseOrder();
+        assertEquals(3, r.getActionPhaseOrder().get(0).getId());
     }
 
     /**
-     * This method tests the correct working of setActionOrder()
+     * This method tests the correct working of setActionPhaseOrder()
      * when there is a game with 2 players.
      */
     @Test
-    void setActionOrderTest2Player() {
+    void setActionPhaseOrderTest2Player() {
         g = new GameConstantsCreatorTwoPlayers();
         GameConstants gc = g.create();
         ArrayList<Player> players = new ArrayList<>();
@@ -99,8 +67,8 @@ class RoundTest {
         players.add(p1);
         players.add(p2);
         r = new Round(players);
-        r.setActionOrder();
-        assertEquals(2, r.getActionOrder().get(0).getId());
+        r.setActionPhaseOrder();
+        assertEquals(2, r.getActionPhaseOrder().get(0).getId());
     }
 
     /**
@@ -153,4 +121,31 @@ class RoundTest {
         assertEquals(27, r.getPlanningPhaseOrder().get(1).getId());
     }
 
+    @Test
+    void nextTurnTest() {
+        g = new GameConstantsCreatorThreePlayers();
+        GameConstants gc = g.create();
+        ArrayList<Player> players = new ArrayList<>();
+        Round r;
+        Player p1 = new Player(1, gc);
+        Player p2 = new Player(2, gc);
+        Player p3 = new Player(3, gc);
+        p1.playCard(4);
+        p2.playCard(0);
+        p3.playCard(8);
+        players.add(p1);
+        players.add(p2);
+        players.add(p3);
+        r = new Round(players);
+        assertEquals(1, r.getCurrentPlayer().getId());
+        r.setPlanningPhaseOrder();      //setPlanningPhaseOrder --> 231
+        assertEquals(2, r.getCurrentPlayer().getId());
+        r.nextTurn();
+        r.nextTurn();
+        assertEquals(1, r.getCurrentPlayer().getId());
+        r.setActionPhaseOrder();        //setActionPhaseOrder --> 213
+        assertEquals(2, r.getCurrentPlayer().getId());
+        r.nextTurn();
+        assertEquals(1,r.getCurrentPlayer().getId());
+    }
 }
