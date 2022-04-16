18acff02a4a5486306d624b8fa1a7ba1644ea8d6
BrescianiLuca <luca5.bresciani@mail.polimi.it>
2022-04-16 13:34:19 +0200

    Create interface PutThreeStudentInTheBag

---
+++ src/main/java/it/polimi/ingsw/model/expertGame/PutThreeStudentsInTheBag.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.model.expertGame;
+
+import it.polimi.ingsw.model.Color;
+
+public interface PutThreeStudentsInTheBag {
+    void putThreeStudentsInTheBag(Color studentColor);
+}
