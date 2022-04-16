c48d3adb5c468bccc4bbaab9c9a3f5149cb6e12c
BrescianiLuca <luca5.bresciani@mail.polimi.it>
2022-04-16 21:13:47 +0200

    Add BannedIslandCard logic

--- src/main/java/it/polimi/ingsw/model/expertGame/BannedIslandCard.java
+++ src/main/java/it/polimi/ingsw/model/expertGame/BannedIslandCard.java
@@ -1,4 +1,22 @@
 package it.polimi.ingsw.model.expertGame;
 
-public class BannedIslandCard {
+//TODO vedere logica con cui l' influenza viene calcolata = 0
+public class BannedIslandCard extends ExpertCard {
+    private final BannedIsland game;
+    private final int islandIndex;
+
+    public BannedIslandCard(int islandIndex, BannedIsland game) {
+        super(2);
+        this.islandIndex = islandIndex;
+        this.game = game;
+    }
+
+    @Override
+    public void effect() {
+        if (!isPlayed()) {
+            played = true;
+            price += 1 ;
+        }
+        game.banIsland(islandIndex);
+    }
 }
