03738255b71d68a314afc126bfcfb2f611aecf6e
Lorenzo Corrado <lorenzo.corrado@mail.polimi.it>
2022-05-03 19:56:53 +0200

    Added login messages

--- src/main/java/it/polimi/ingsw/server/GameHandler.java
+++ src/main/java/it/polimi/ingsw/server/GameHandler.java
@@ -1,7 +1,10 @@
 package it.polimi.ingsw.server;
 
+import java.util.ArrayList;
+
 public class GameHandler {
     private final int numPlayer;
+    private ArrayList<int> playersConnections;
     public GameHandler(int numPlayer) {
         this.numPlayer = numPlayer;
     }
