75673de6a6278ac7dd4d85ca70ebb27c38581029
Lorenzo Corrado <lorenzo.corrado@mail.polimi.it>
2022-05-03 18:08:02 +0200

    Added login messages

---
+++ src/main/java/it/polimi/ingsw/messages/Disconnect.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.messages;
+
+/**
+ * This class is a message client to server that indicate a player has disconnected
+ * @author Lorenzo Corrado
+ */
+public class Disconnect implements Message{
+}
---
+++ src/main/java/it/polimi/ingsw/messages/NumberOfPlayers.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.messages;
+
+/**
+ *  This class is a message client to server that provides the number of players chosen by the first player
+ *  @author Lorenzo Corrado
+ */
+public class NumberOfPlayers implements Message {
+    private final int numPlayer;
+
+    public NumberOfPlayers(int num){
+        this.numPlayer = num;
+    }
+
+    /**
+     *
+     * @return the number of players
+     */
+    public int getNumPlayer() {
+        return numPlayer;
+    }
+
+}
