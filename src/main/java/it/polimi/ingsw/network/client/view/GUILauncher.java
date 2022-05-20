92eb08bccc6db2c8c6713d530f2511f1cfeed478
BrescianiLuca <luca5.bresciani@mail.polimi.it>
2022-05-21 00:36:15 +0200

    Added GUI class

---
+++ src/main/java/it/polimi/ingsw/network/client/view/GUILauncher.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.network.client.view;
+
+import javafx.application.Application;
+
+public class GUILauncher {
+    public static void main(String args[]){
+        Application.launch(GUI.class);
+    }
+}
